6566281041e1246122aff9dddbb80830ab69f470
superjiangjiang <1049852403@qq.com>
2019-07-03 15:47:21 +0800

    还差一个统计

--- src/components/teacher/StatisticsStudentEvaluation.vue
+++ src/components/teacher/StatisticsStudentEvaluation.vue
@@ -1,15 +1,134 @@
 <template>
     <div>
-        <h1>这是学生评价的统计组件</h1>
+      <el-breadcrumb separator-class="el-icon-arrow-right" class="user-breadcrumb">
+        <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>教师</el-breadcrumb-item>
+        <el-breadcrumb-item>学生评价统计</el-breadcrumb-item>
+      </el-breadcrumb>
+
+      <el-table :data="tableData" stripe>
+        <el-table-column prop="number" label="教师号" width="180">
+        </el-table-column>
+        <el-table-column prop="teachername" label="教师名" width="180">
+        </el-table-column>
+        <el-table-column prop="subject" label="所教科目" width="180">
+        </el-table-column>
+        <el-table-column prop="score" label="评价得分" width="180">
+        </el-table-column>
+      </el-table>
+
+      <el-pagination background layout="prev, pager, next" :total="total" :page-size="pageSize" :current-page.sync="curPage" @current-change="changePage">
+      </el-pagination>
     </div>
 </template>
 
 <script>
     export default {
-        name: "StatisticsStudentEvaluation"
+        name: "StatisticsStudentEvaluation",
+      created() {
+        // console.log('axios: ', this.$http === axios)
+
+        // 发送请求，获取数据
+        // this.getUserList()
+      },
+
+      data() {
+        return {
+          userList: [],
+          // 每页大小
+          pageSize: 3,
+          // 当前页码
+          curPage: 1,
+          // 总条数
+          total: 0,
+          // 搜索内容
+          queryStr: '',
+          //假数据
+          tableData: [{
+            number: '201603091071',
+            teachername: '赵珂',
+            subject: '数据结构',
+            score:100
+          }, {
+            number: '201603091071',
+            teachername: '赵珂',
+            subject: '数据结构',
+            score:100
+          },{
+            number: '201603091071',
+            teachername: '赵珂',
+            subject: '数据结构',
+            score:100
+          },{
+            number: '201603091071',
+            teachername: '赵珂',
+            subject: '数据结构',
+            score:100
+          },{
+            number: '201603091071',
+            teachername: '赵珂',
+            subject: '数据结构',
+            score:100
+          },{
+            number: '201603091071',
+            teachername: '赵珂',
+            subject: '数据结构',
+            score:100
+          },{
+            number: '201603091071',
+            teachername: '赵珂',
+            subject: '数据结构',
+            score:100
+          }]
+
+        }
+      },
+
+      methods: {
+        // 获取用户列表数据
+        // curPage = 1 给参数添加默认值
+        /* getUserList(curPage = 1) {
+          this.$http
+            .get('/student', {
+              params: {
+                // 当前页
+                pagenum: curPage,
+                // 每页展示多少条数据
+                pagesize: 3,
+                // 查询条件
+                query: this.queryStr || ''
+              }
+              // 将 token 作为请求头，传递给服务器接口
+              // 这样，才能正确的调用这个接口
+              // headers: {
+              //   Authorization: localStorage.getItem('token')
+              // }
+            })
+            .then(res => {
+              console.log('请求成功:', res)
+              const { data, meta } = res.data
+              if (meta.status === 200) {
+                // 获取数据成功
+                this.userList = data.student
+                this.total = data.total
+                this.curPage = data.pagenum
+              }
+            })
+        }, */
+        /**
+         * 分页获取数据
+         * 参数 cruPage 表示当前点击的页码
+         */
+
+      }
     }
 </script>
 
 <style scoped>
-
+  .user-breadcrumb {
+    line-height: 40px;
+    background-color: #d4dae0;
+    font-size: 18px;
+    padding-left: 10px;
+  }
 </style>
